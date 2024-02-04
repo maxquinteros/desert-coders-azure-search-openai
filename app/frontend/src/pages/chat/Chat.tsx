5cf75e405f23dde8a1eeeca836a7643423c03cbb
Maximiliano Quinteros Cortés <maximiliano.quinteros.cortes@hotmail.com>
2024-02-04 23:41:11 +0000

    Co-authored-by: joseph  flores <joseph.flores@alumnos.ucn.cl> Co-authored-by: Pablo Gallardo <PabloNAGG@users.noreply.github.com>

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -270,7 +270,7 @@ const Chat = () => {
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
                             <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" />
-                            <h1 className={styles.chatEmptyStateTitle}>Chat about California plants!</h1>
+                            <h1 className={styles.chatEmptyStateTitle}>Hola amigos de youtube</h1>
                             <h2 className={styles.chatEmptyStateSubtitle}>Ask anything or try an example</h2>
                             <ExampleList onExampleClicked={onExampleClicked} useGPT4V={useGPT4V} />
                         </div>
