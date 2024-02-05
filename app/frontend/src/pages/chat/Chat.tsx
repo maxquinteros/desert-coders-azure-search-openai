2a5af63d553b91ca61a365e7a29b4d44499e0c7e
Maximiliano Quinteros Cortés <maximiliano.quinteros.cortes@hotmail.com>
2024-02-05 01:07:54 +0000

    Your branch and 'origin/main' have diverged,  and have 6 and 4 different commits each, respectively.
     Changes to be committed:
    	modified:   app/frontend/src/pages/chat/Chat.tsx
    	new file:   data/OIG3.jpeg

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -1,6 +1,6 @@
 import { useRef, useState, useEffect } from "react";
 import { Checkbox, Panel, DefaultButton, TextField, SpinButton } from "@fluentui/react";
-import { SparkleFilled } from "@fluentui/react-icons";
+import { CameraSparkles20Regular, SparkleFilled } from "@fluentui/react-icons";
 import readNDJSONStream from "ndjson-readablestream";
 
 import styles from "./Chat.module.css";
@@ -269,7 +269,8 @@ const Chat = () => {
                 <div className={styles.chatContainer}>
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
-                            <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" />
+                            <SparkleFilled fontSize={"120px"} primaryFill={"rgba(58, 195, 99, 1)"} aria-hidden="true" aria-label="Chat logo" />
+                            <img src="/workspaces/azure-search-openai-demo/data/OIG3.jpeg" />
                             <h1 className={styles.chatEmptyStateTitle}>Hola amigos</h1>
                             <h2 className={styles.chatEmptyStateSubtitle}>Ask anything or try an example</h2>
                             <ExampleList onExampleClicked={onExampleClicked} useGPT4V={useGPT4V} />
